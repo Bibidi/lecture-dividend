9848d6033507ed609eba9743c8a9ef6cedb1c826
Bibidi <byungdeuk.kim@gmail.com>
2022-12-12 03:19:48 +0900

    Init Project
    * 프로젝트 세팅

---
+++ src/test/java/com/example/lecturediviend/LectureDiviendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecturediviend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureDiviendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
