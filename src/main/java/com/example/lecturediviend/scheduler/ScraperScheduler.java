df47200d0b3d90a481c2b7fc472fa4aaca88dde7
Bibidi <byungdeuk.kim@gmail.com>
2022-12-16 14:11:45 +0900

    Implement Redis cache

--- src/main/java/com/example/lecturediviend/scheduler/ScraperScheduler.java
+++ src/main/java/com/example/lecturediviend/scheduler/ScraperScheduler.java
@@ -2,6 +2,7 @@ package com.example.lecturediviend.scheduler;
 
 import com.example.lecturediviend.model.Company;
 import com.example.lecturediviend.model.ScrapedResult;
+import com.example.lecturediviend.model.constant.CacheKey;
 import com.example.lecturediviend.persist.CompanyRepository;
 import com.example.lecturediviend.persist.DividendRepository;
 import com.example.lecturediviend.persist.entity.CompanyEntity;
@@ -9,15 +10,17 @@ import com.example.lecturediviend.persist.entity.DividendEntity;
 import com.example.lecturediviend.scraper.Scraper;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Slf4j
 @Component
 @AllArgsConstructor
+@EnableCaching
 public class ScraperScheduler {
 
     private final CompanyRepository companyRepository;
@@ -26,33 +29,19 @@ public class ScraperScheduler {
 
     private final Scraper financeScraper;
 
-    @Scheduled(fixedDelay = 1000)
-    public void test1() throws InterruptedException {
-        Thread.sleep(5000);
-        System.out.println(Thread.currentThread().getName() + " -> 테스트 1 : " + LocalDateTime.now());
-    }
-
-    @Scheduled(fixedDelay = 1000)
-    public void test2() throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + " -> 테스트 2 : " + LocalDateTime.now());
-    }
-
-
-    // 일정 주기마다 수행
-//    @Scheduled(cron = "${scheduler.scrap.yahoo}")
+    @CacheEvict(value = CacheKey.KEY_FINANCE, allEntries = true)
+    @Scheduled(cron = "${scheduler.scrap.yahoo}")
     public void financeScheduling() {
-//        log.info("scraping scheduler is started");
+        log.info("scraping scheduler is started");
         // 저장된 회사 목록을 조회
         List<CompanyEntity> companyEntities = companyRepository.findAll();
 
         // 회사마다 배당금 정보를 새로 스크래핑
         companyEntities.forEach(companyEntity -> {
             log.info("scraping scheduler is started -> " + companyEntity.getName());
-            ScrapedResult scrapedResult = financeScraper.scrap(Company.builder()
-                    .name(companyEntity.getName())
-                    .ticker(companyEntity.getTicker())
-                    .build());
+            ScrapedResult scrapedResult = financeScraper.scrap(new Company(companyEntity.getName(), companyEntity.getTicker()));
 
+            // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
             scrapedResult.getDividends().stream()
                     .map(dividend -> new DividendEntity(companyEntity.getId(), dividend))
                     .forEach(dividendEntity -> {
@@ -62,13 +51,12 @@ public class ScraperScheduler {
                         }
                     });
 
+            // 연속적으로 스크래핑 대상 사이트 서버에 요청을 날리지 않도록 일시정지
             try {
                 Thread.sleep(3000);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
         });
-
-        // 스크래핑한 배당금 정보 중 데이터베이스에 없는 값은 저장
     }
 }
--- src/main/java/com/example/lecturediviend/service/FinanceService.java
+++ src/main/java/com/example/lecturediviend/service/FinanceService.java
@@ -3,16 +3,20 @@ package com.example.lecturediviend.service;
 import com.example.lecturediviend.model.Company;
 import com.example.lecturediviend.model.Dividend;
 import com.example.lecturediviend.model.ScrapedResult;
+import com.example.lecturediviend.model.constant.CacheKey;
 import com.example.lecturediviend.persist.CompanyRepository;
 import com.example.lecturediviend.persist.DividendRepository;
 import com.example.lecturediviend.persist.entity.CompanyEntity;
 import com.example.lecturediviend.persist.entity.DividendEntity;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class FinanceService {
@@ -20,7 +24,9 @@ public class FinanceService {
     private final CompanyRepository companyRepository;
     private final DividendRepository dividendRepository;
 
+    @Cacheable(key = "#companyName", value = CacheKey.KEY_FINANCE)
     public ScrapedResult getDividendByCompanyName(String companyName) {
+        log.info("search company -> " + companyName);
         // 회사명을 기준으로 회사 정보 조회
         CompanyEntity companyEntity = companyRepository.findByName(companyName)
                 .orElseThrow(() -> new RuntimeException("존재하지 않는 회사명입니다"));
@@ -30,16 +36,9 @@ public class FinanceService {
 
         // 결과 조합 후 반환
         List<Dividend> dividends = dividendEntities.stream()
-                .map(dividendEntity -> Dividend.builder()
-                        .date(dividendEntity.getDate())
-                        .dividend(dividendEntity.getDividend())
-                        .build())
+                .map(dividendEntity -> new Dividend(dividendEntity.getDate(), dividendEntity.getDividend()))
                 .collect(Collectors.toList());
 
-        return new ScrapedResult(Company.builder()
-                .ticker(companyEntity.getTicker())
-                .name(companyEntity.getName())
-                .build(),
-                dividends);
+        return new ScrapedResult(new Company(companyEntity.getTicker(), companyEntity.getName()), dividends);
     }
 }
